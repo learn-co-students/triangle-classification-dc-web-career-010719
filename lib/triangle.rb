c783fa093a6c3f3f600b2de9f1f9c27a0c43cbfb
shannon-nabors <shannon.nabors@gmail.com>
2019-01-12 10:51:43 -0500

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+
+  attr_reader :side_a, :side_b, :side_c
+  attr_accessor :sides
+
+  def initialize(side_a, side_b, side_c)
+    @side_a, @side_b, @side_c = side_a, side_b, side_c
+    @sides = [@side_a, @side_b, @side_c]
+  end
+
+  def kind
+    if self.sides.uniq.size == 1 && self.sides.min > 0
+      :equilateral
+    elsif self.sides.sort.first(2).inject(:+) <= self.sides.sort.pop || self.sides.min <= 0
+      begin
+        raise TriangleError
+      end
+    elsif self.sides.uniq.size == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
