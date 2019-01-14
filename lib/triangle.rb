a572b5323dc5130dd3277369477d723d89cde3aa
Dusty211 <kylebenz@hotmail.com>
2019-01-14 17:49:52 -0500

    All tests passing.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+
 class Triangle
-  # write code here
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @side_array = [@side_1, @side_2, @side_3]
+  end
+
+  def unique_sides
+    to_float = @side_array.collect &:to_f  #Array.class.uniq == 1
+    to_float.uniq.count
+  end
+
+  def kind
+    if invalid_triangle?
+      raise TriangleError
+    else
+      return :equilateral if unique_sides == 1
+      return :isosceles if unique_sides == 2
+      return :scalene if unique_sides == 3
+    end
+  end
+
+  def invalid_triangle?
+    return true if @side_array.any? {|side| side == 0}
+    return true if @side_1 + @side_2 <= @side_3
+    return true if @side_1 + @side_3 <= @side_2
+    return true if @side_3 + @side_2 <= @side_1
+    return false
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
